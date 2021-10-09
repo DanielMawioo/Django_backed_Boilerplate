17db018f11e40d5b35df286626c22b5ae4d5dc6a
DanielMawioo <mawioodaniel43@gmail.com>
2021-10-09 13:59:01 +0300

    postgres settings

--- backendapi/settings.py
+++ backendapi/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'backendapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# Postgresql database set up
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+      'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'postgres',
+         'USER': 'postgres',
+         'PASSWORD': 'postgres',
+         'HOST': 'db',
+         'PORT': 5432,
+     }
+ }
 
 
 # Password validation
